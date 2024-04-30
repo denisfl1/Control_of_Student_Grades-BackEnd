cf79f6c9cb965fc279f1bd2a00105dca1764c28d
denisfl1 <denisfl1@hotmail.com>
2024-04-30 20:47:41 -0300

    update

---
+++ app/controllers/student_controller.rb
@@ -0,0 +1,44 @@
+class StudentsController < ApplicationController
+
+
+  def createStudent
+
+    names = [
+      'Aurora Silva',
+      'Dante Rodrigues',
+      'Valentina Oliveira',
+      'Mateus Santos',
+      'Isabela Pereira',
+      'Gabriel Costa',
+      'Helena Almeida',
+      'Lucas Ferreira',
+      'Sofia Cardoso',
+      'Miguel Gonçalves',
+    ]
+
+    names.each{|data|
+
+    name = data
+    parts = name.split(' ')
+    first = parts.first
+    last = parts.last
+    random = rand(* 1000000000)
+
+    Student.create(name:first,surname:last,ra:random)
+
+    }
+
+
+  end
+  render json: "Criado com sucesso",status:200
+
+
+  def getStudents
+
+    students = Student.all
+
+    render json: students, status:200
+
+  end
+
+end
